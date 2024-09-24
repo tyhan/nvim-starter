36a406bda3f7592e0a00570a70e012a3d5ffe5ff
tyhan <tyhan@npixel.co.kr>
2024-09-24 18:20:45 +0900

    Add terraform ls

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -100,6 +100,7 @@ return {
               },
             },
           },
+          terraformls = {},
         },
         -- you can do any additional lsp server setup here
         -- return true if you don't want this server to be setup with lspconfig
